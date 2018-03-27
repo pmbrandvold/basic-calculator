c957435ec5da0bd5f3c54926173f5ff3dc7b42a1
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-03-27 15:54:31 -0500

    Functions now work with 2 or more numbers

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-function add(a, b) {
-  return a + b;
+function add(numbers) {
+  let sum = 0;
+  numbers.forEach(number => {
+    sum += number;
+  });
+  return sum;
 }
 
-function subtract(a, b) {
-  return a - b;
+function subtract(numbers) {
+  let diff = numbers[0];
+  for (i = 0; i < numbers.length-1; i++) {
+    diff -= numbers[i+1]
+  }
+  return diff;
 }
 
-function multiply(a, b) {
-  return a * b;
+function multiply(numbers) {
+  let product = 1;
+  numbers.forEach(number => product *= number);
+  return product;
 }
 
-function divide(a, b) {
-  return a / b;
+function divide(numbers) {
+  let quotient = numbers[0];
+  for (i = 0; i < numbers.length-1; i++) {
+    quotient /= numbers[i+1]
+  }
+  return quotient;
 }
 
 function operate() {

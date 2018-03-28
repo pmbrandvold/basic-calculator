58e5b4797b255dece845ed69da8405fdb83560dc
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-03-28 15:07:26 -0500

    Takes two numbers and performs specified operation.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
-function operate(operator, numbers) {
-  
+function operate(operator, firstNum, secNum) {
+  let answer;
+  switch (operator) {
+    case 'add':
+      return add([firstNum, secNum]);
+    case 'subtract':
+      return subtract([firstNum, secNum]);
+    case 'multiply':
+      return multiply([firstNum, secNum]);
+    case 'divide':
+      return divide([firstNum, secNum]);
+  }
 }

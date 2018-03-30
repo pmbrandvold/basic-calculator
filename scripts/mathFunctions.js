06aee0ab7ac65d93fdca947571f00ebceaa99927
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-03-30 11:57:25 -0500

    Remove unnecessary complexity

--- scripts/mathFunctions.js
+++ scripts/mathFunctions.js
@@ -1,29 +1,17 @@
-function add(numbers) {
-  let sum = 0;
-  numbers.forEach(number => sum += number);
-  return sum;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(numbers) {
-  let diff = numbers[0];
-  for (i = 0; i < numbers.length-1; i++) {
-    diff -= numbers[i+1]
-  }
-  return diff;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(numbers) {
-  let product = 1;
-  numbers.forEach(number => product *= number);
-  return product;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(numbers) {
-  let quotient = numbers[0];
-  for (i = 0; i < numbers.length-1; i++) {
-    quotient /= numbers[i+1]
-  }
-  return quotient;
+function divide(a, b) {
+  return a / b;
 }
 
 module.exports = {
--- tests/mathFunctions.spec.js
+++ tests/mathFunctions.spec.js
@@ -2,45 +2,54 @@ var calculator = require ('../scripts/mathFunctions.js');
 
 describe('add', function() {
 	it('adds 0 and 0', function() {
-		expect(calculator.add([0,0])).toEqual(0);
+		expect(calculator.add(0, 0)).toEqual(0);
 	});
 	it('adds 2 and 2', function() {
-		expect(calculator.add([2,2])).toEqual(4);
+		expect(calculator.add(2, 2)).toEqual(4);
 	});
-	it('adds positive numbers', function() {
-		expect(calculator.add([2,6])).toEqual(8);
+	it('adds large numbers', function() {
+		expect(calculator.add(2000, 1505)).toEqual(3505);
 	});
-  it('adds many positive numbers', function() {
-    expect(calculator.add([2,6,3,7,10])).toEqual(28);
-  });
+	it('add negative numbers', function() {
+		expect(calculator.add(10, -3)).toEqual(7);
+	});
 });
 
 describe('subtract', function() {
 	it('subtracts numbers', function() {
-		expect(calculator.subtract([10,4])).toEqual(6);
+		expect(calculator.subtract(10, 4)).toEqual(6);
+	});
+	it('subtracts large numbers', function() {
+		expect(calculator.subtract(1035, 979)).toEqual(56);
+	});
+	it('subtracts larger numbers from smaller numbers', function() {
+		expect(calculator.subtract(100, 500)).toEqual(-400);
 	});
 });
 
 describe('multiply', function() {
 	it('multiplies two numbers', function() {
-		expect(calculator.multiply([2,4])).toEqual(8);
+		expect(calculator.multiply(2, 4)).toEqual(8);
 	});
-	it('multiplies several numbers', function() {
-		expect(calculator.multiply([2,4,6,8,10,12,14])).toEqual(645120);
+	it('multiplies large numbers', function() {
+		expect(calculator.multiply(1000, 100)).toEqual(100000);
 	});
   it('multiplies a negative and a positive', function() {
-    expect(calculator.multiply([-2, 5])).toEqual(-10);
+    expect(calculator.multiply(-2, 5)).toEqual(-10);
   });
   it('multiplies two negative numbers', function() {
-    expect(calculator.multiply([-5, -6])).toEqual(30);
+    expect(calculator.multiply(-5, -6)).toEqual(30);
   })
 });
 
 describe('divide', function() {
   it('divides two numbers', function() {
-    expect(calculator.divide([4, 2])).toEqual(2);
+    expect(calculator.divide(4, 2)).toEqual(2);
   });
-  it('divides several numbers', function() {
-    expect(calculator.divide([16, 2, 4])).toEqual(2);
+  it('divides large numbers', function() {
+    expect(calculator.divide(5000, 160)).toEqual(31.25);
   });
+	it('divides a large number into a small number', function() {
+		expect(calculator.divide(10, 100)).toEqual(0.1);
+	});
 });
